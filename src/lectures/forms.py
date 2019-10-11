bb0e659c242ccf9e42b33b5f49264655209a112c
Cave <FALBELLAIHI@MAIL.UMW.EDU>
2019-10-11 14:46:40 +0300

    removed unecessary files

--- src/lectures/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-# NONE FOR NOW
-
-class VideoForm(forms.Form):
-	# THIS WILL HANDLE THE VIDEO REMINING TIME TO WATCH 
-	video_id = forms.IntegerField()
-	remining = forms.IntegerField()
-
